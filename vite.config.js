7fd3344d07a3d5c4fb4c949d31160d989e6d45ee
edch <soderblomed@gmail.com>
2024-11-22 11:45:54 +0100

    remove config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/just-todo-it/",
-});
+// export default defineConfig({
+//   base: "/just-todo-it/",
+// });
